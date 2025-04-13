24216d14c780099a387197e1996186e44355dddb
Ar4sCode <ar4sazizi@gmail.com>
2025-04-13 20:02:24 +0330

    Add fixed navbar for all pages and create a route to access all pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router";
+
+const Header = () => {
+  return (
+    <div className="bg-white flex justify-between items-center p-5 font-sans">
+      <Link className="font-extrabold text-2xl" to="/">#VANLIFE</Link>
+      <div className=" flex gap-5">
+        <Link className="text-gray-400 font-semibold" to="about">About</Link>
+        <Link className="text-gray-400 font-semibold" to="vans">Vans</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
