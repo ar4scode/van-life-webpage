93e5f1f604566fa2920b56b38b095f36308bff81
Ar4sCode <ar4sazizi@gmail.com>
2025-04-29 16:12:36 +0330

    Change the grid layout for mobile users and add header text to van component

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -26,19 +26,20 @@ const Vans = () => {
   return (
 
     <>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 pt-20 p-5 pb-20">
+      <h1 className="pt-20 font-bold text-3xl px-4">Explore our van options</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-10 pt-10 p-5 pb-20 fonts">
         {vansData.map((van) => {
           return (
             <div key={van.id}>
               <img src={van.imageUrl} className="rounded-md" alt="" />
-              <div className="flex justify-between">
+              <div className="flex flex-col gap-2 md:flex-row md:justify-between">
                 <h1 className="text-nowrap font-bold pt-2 md:text-xl lg:text-2xl">{van.name}</h1>
                 <div className="flex flex-col relative pt-2">
                   <p className="font-bold lg:text-xl">${van.price}</p>
                   <p className="text-sm text-gray-500 absolute top-6 lg:top-7 left-3">/day</p>
                 </div>
               </div>
-              <p className={`font-semibold text-white max-w-20 text-center p-1 rounded-md mt-2 ${getTypeClass(van.type)}`}>
+              <p className={`font-semibold text-white max-w-20 text-center p-1 rounded-md mt-5 ${getTypeClass(van.type)}`}>
                 {van.type}
               </p>
 
