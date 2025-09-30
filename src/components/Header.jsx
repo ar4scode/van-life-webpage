8981d1b6db3afaaccbe467bd45eb968389c3d078
Ar4sCode <ar4sazizi@gmail.com>
2025-09-30 11:45:38 +0330

    Change Link to Navlink , in navlink we can use callback function in classname for styling the link when is active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="fixed z-10 right-0 left-0 bg-white flex justify-between items-center p-5 font-sans">
-      <Link className="font-extrabold text-2xl" to="/">#VANLIFE</Link>
+      <NavLink className="font-extrabold text-2xl" to="/">#VANLIFE</NavLink>
       <div className=" flex gap-5">
-        <Link className="text-gray-400 font-semibold" to="/host">Host</Link>
-        <Link className="text-gray-400 font-semibold" to="/about">About</Link>
-        <Link className="text-gray-400 font-semibold" to="/vans">Vans</Link>
+        <NavLink className={({isActive}) => isActive ? "text-black underline font-semibold" : "text-gray-400 font-semibold hover:text-black duration-200" } to="/host">Host</NavLink>
+        <NavLink className={({isActive}) => isActive ? "text-black underline font-semibold" : "text-gray-400 font-semibold hover:text-black duration-200" } to="/about">About</NavLink>
+        <NavLink className={({isActive}) => isActive ? "text-black underline font-semibold" : "text-gray-400 font-semibold hover:text-black duration-200" } to="/vans">Vans</NavLink>
       </div>
     </div>
   )
--- src/components/Host/HostNavbar.jsx
+++ src/components/Host/HostNavbar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 const HostNavbar = () => {
   return (
     <div className="pt-20 flex ml-2 gap-3 font-semibold text-gray-400 md:gap-5 md:ml-10">
-      <Link to="/host">Dashboard</Link>
-      <Link to="/host/income">Income</Link>
-      <Link to="/vans">Vans</Link>
-      <Link to="/host/reviews">Reviews</Link>
+      <NavLink end className={({isActive}) => isActive ? "text-black underline" : "duration-200 hover:text-black"} to="/host">Dashboard</NavLink>
+      <NavLink className={({isActive}) => isActive ? "text-black underline" : "duration-200 hover:text-black"} to="/host/income">Income</NavLink>
+      <NavLink className={({isActive}) => isActive ? "text-black underline" : "duration-200 hover:text-black"} to="/vans">Vans</NavLink>
+      <NavLink className={({isActive}) => isActive ? "text-black underline" : "duration-200 hover:text-black"} to="/host/reviews">Reviews</NavLink>
     </div>
   )
 }
